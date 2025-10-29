32927317738509725521b78a9c6e05c9c1b50336
l18707977088 <l18707977088@163.com>
2025-10-29 16:47:55 +0800

    init

---
+++ src/command_manager.cpp
@@ -0,0 +1,49 @@
+#include "command_manager.h"
+#include "context.h"
+
+namespace toy2d {
+
+    CommandManager::CommandManager() {
+        pool_ = createCommandPool();
+    }
+
+    CommandManager::~CommandManager() {
+        auto& ctx = Context::Getinstance();
+        ctx.device.destroyCommandPool(pool_);
+    }
+
+    void CommandManager::ResetCmds() {
+        Context::Getinstance().device.resetCommandPool(pool_);
+    }
+
+    vk::CommandPool CommandManager::createCommandPool() {
+        auto& ctx = Context::Getinstance();
+
+        vk::CommandPoolCreateInfo createInfo;
+
+        createInfo.setQueueFamilyIndex(ctx.queueFamilyIndices.graphicsFamily.value())               //
+            .setFlags(vk::CommandPoolCreateFlagBits::eResetCommandBuffer);
+
+        return ctx.device.createCommandPool(createInfo);
+    }
+
+    std::vector<vk::CommandBuffer> CommandManager::CreateCommandBuffers(std::uint32_t count) {
+        auto& ctx = Context::Getinstance();
+
+        vk::CommandBufferAllocateInfo allocInfo;
+        allocInfo.setCommandPool(pool_)
+            .setCommandBufferCount(1)
+            .setLevel(vk::CommandBufferLevel::ePrimary);
+
+        return ctx.device.allocateCommandBuffers(allocInfo);
+    }
+
+    vk::CommandBuffer CommandManager::CreateOneCommandBuffer() {
+        return CreateCommandBuffers(1)[0];
+    }
+
+    void CommandManager::FreeCmd(vk::CommandBuffer buf) {
+        Context::Getinstance().device.freeCommandBuffers(pool_, buf);
+    }
+
+}
